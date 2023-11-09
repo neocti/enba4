a592fc078e8aad9d30246f3b536ae350f520e9c4
Diego Cruz <cruzdiegooo@gmail.com>
2023-11-09 21:40:59 +0800

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.deepOrange,
         appBar: AppBar(
           title: const Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.deepOrange,
         ),
         body: DicePage(),
       ),
@@ -23,41 +23,112 @@ class DicePage extends StatefulWidget {
   State<DicePage> createState() => _DicePageState();
 }
 class _DicePageState extends State<DicePage> {
-  int left = 1, right = 1;
+  int left = 1, right = 1, sum = 0, leftScore = 0, rightScore = 0;
   @override
   Widget build(BuildContext context) {
+    sum = left + right;
+    score();
     return Center(
-        child: Row(
-          children: [
-            Expanded(child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      ChangeValue();
-                    });
-                  },
-                  child: Image.asset('images/dice$left.png')),
-            )),
-            Expanded(child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                     ChangeValue();
-                    });
-
-                  },
-                  child: Image.asset('images/dice$right.png')),
-            )),
-          ],
-        ));
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 0.0,
+                      horizontal: 16.0),
+                  child: Text("Score: $leftScore", style: const TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 25.0,
+                  ),)
+              )),
+              Expanded(child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 0.0,
+                    horizontal: 16.0),
+                child: Text("Score: $rightScore", style: const TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 25.0
+                )),
+              ))
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        changeValue();
+                      });
+                    },
+                    child: Image.asset('images/dice$left.png'),
+                  )
+              )),
+              Expanded(child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        changeValue();
+                      });
+                    }, child: Image.asset('images/dice$right.png'),
+                  )
+              )),
+            ],
+          ),
+          Text("The total roll is $sum",
+              style: const TextStyle(
+                fontSize: 25.0,
+                fontFamily: "Poppins",
+              )),
+          Text(getWinner(),
+              style: const TextStyle(
+                fontSize: 25.0,
+                fontFamily: "Poppins",
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal:
+            0.0),
+            child: FilledButton(onPressed: () {
+              setState(() {
+                left = 1;
+                right = 1;
+                leftScore = 0;
+                rightScore = 0;
+              });
+            },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ), child: const Text(
+                  "Reset Score"
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
-    void ChangeValue(){
-      left = 1 + Random().nextInt(6);
-      right = 1 + Random().nextInt(6);
+  void changeValue() {
+    left = 1 + Random().nextInt(6);
+    right = 1 + Random().nextInt(6);
+  }
+  String getWinner() {
+    if (left > right) {
+      return "Left dice rolls higher";
+    } else if (right > left) {
+      return "Right dice rolls higher";
+    } else {
+      return "Dice rolls are equal";
     }
   }
-
+  void score() {
+    if (left > right) {
+      leftScore++;
+    } else if (right > left) {
+      rightScore++;
+    }
+  }
+}
 
 
